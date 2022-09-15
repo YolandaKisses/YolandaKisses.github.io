7e6fb2f49e990755693b7a7fa8e1717e4679f6cd
YolandaKisses <jelare_svken@qq.com>
2022-09-15 10:29:42 +0800

    fix

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# 忽略错误
+set -e
+
+# 进入待发布的目录
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy page'
+
+# 如果部署到 https://<USERNAME>.github.io
+git push -f git@github.com:YolandaKisses/YolandaKisses.github.io.git main
+
+cd -
